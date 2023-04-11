794e2e09b941aaa3ec1847432b67fca1b1d48ea5
Chezy Dadang <cheezydadang@gmail.com>
2023-04-11 17:18:56 +0800

    I Updated the LogIn component and App.js.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import LogIn from './components/LogIn';
+import SignUpForm from './components/SignUpForm';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <div className="App p-36">
       <div>
-        <LogIn></LogIn>
+        <LandingPage/>
+      </div>
+      <div>
+        <SignUpForm/>
+      </div>
+      <div>
+        <LogIn/>
       </div>
     </div>
   );
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -2,19 +2,25 @@ import React from 'react'
 
 function LogIn() {
   return (
-        <div> 
-            <div className="card bg-accent w-96">
-                <div className="card-body">
-                    <input type="text" placeholder="Email Address" className="input w-full max-w-xs" />
-                    <input type="text" placeholder="Password" className="input w-full max-w-xs" />
-                    <a className= "text-white">Forgot your password?</a>
+    <div className='p-10 flex justify-center'>
+        <div className="card card-compact w-96 bg-base-200 shadow-xl">
+            <div className="card-body">
+            <   h2 className="card-title justify-center text-purple-500 py-2">Welcome to Asdang App!</h2>
+                <div className='py-2'>
+                    <label className="label label-text">Email Address</label>
+                    <input type="text" placeholder="Email Address" className="input input-bordered border-purple-300 w-full max-w-xs" />
+                </div>
+                <div className='py-2'>
+                    <label className="label label-text">Password</label>
+                    <input type="text" placeholder="Password" className="input input-bordered border-purple-300 w-full max-w-xs" />
+                </div>
+                <a className='text-purple-400'>Forgot Password?</a>
                 <div className="card-actions justify-center">
-                    <button className="btn btn-wide">Log In</button>
+                    <button className=" btn border-purple-300 bg-purple-500 btn-wide">Log In</button>
                 </div>
             </div>
         </div>
     </div>
   )
 }
-
-export default LogIn
+export default LogIn;
